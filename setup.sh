df206808fb435f99a5da934fedffff2e2e1c585b
João Hudson <joao.hudson@taking.com.br>
2024-05-03 13:53:00 -0300

    fix setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
+minikube start --driver=docker --ports=8080:80,30082:30082
+eval $(minikube docker-env)
 docker build -t sample-app ./app/
-docker run -d -p 8089:8089 -e PRIVATE_VALUE='*****' -e CONFIG_VALUE='8089' sample-app
-#kubectl apply -f k8s.yaml
